9f570caf5ebf13a912d74a6d3f403d05f50da339
ChinaYang666 <110529636+ChinaYang666@users.noreply.github.com>
2024-08-06 22:17:35 +0800

    Delete kitti_tools directory

--- kitti_tools/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yang/Code/catkin_ws/src/kitti_tools
-# Build directory: /home/yang/Code/catkin_ws/src/kitti_tools/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
